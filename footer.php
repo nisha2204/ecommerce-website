69eb2533683a3d5b5a89bbb4caee02bf49fb4854
nisha modani <modaninisha@gmail.com>
2021-06-27 00:17:56 +0530

    added all files

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer>     
+    <div class="container">                      
+            <p>Copyright &copy; Lifestyle Store. All Rights Reserved  |  Contact Us: +91 90000 00000</p>              
+    </div> 
+</footer> 
+ 
